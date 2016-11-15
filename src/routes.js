ea79770b170bf3dea56293c4f517a4450ce79985
Von Carcha <vhoncarcha420@gmail.com>
2016-11-15 15:52:14 +0800

    testing

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
-import CoursePage from './components/course/CoursePage';
+import CoursePage from './components/course/CoursesPage';
 
 export default (
   <Route path="/" component={App}>
